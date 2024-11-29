3b170f38c3dd07a15c349e81313b21dce6bc331f
gampnico <45390064+gampnico@users.noreply.github.com>
2024-11-29 10:30:44 +0100

    refactor: minimal working build script

--- src/patch_wrfxcspy/build_wrf.sh
+++ src/patch_wrfxcspy/build_wrf.sh
@@ -7,8 +7,7 @@ USER_PATH="/home/atuin/<group>/<account>/software"
 
 # Load HPC modules
 module purge
-module load hdf5/1.10.7-impi-intel netcdf-c netcdf-fortran/4.5.3-intel gcc/9.4.0 cmake
-module load mkl/2022.1.0 m4/1.4.19
+module load intelmpi/2021.6.0 intel/2021.4.0
 module load user-spack
 module load time
 
@@ -16,8 +15,6 @@ module load time
 export COSIPY_API="${USER_PATH}/path/to/WRF/COUPLER"
 export COSIPY_DIR="${USER_PATH}/path/to/WRF/cosipy"
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${COSIPY_API}
-export LIBRARY_PATH=${LIBRARY_PATH}:${COSIPY_API}
-export PATH=${PATH}:${COSIPY_API}
 
 # WRF options
 export WRFIO_NCD_LARGE_FILE_SUPPORT=1
@@ -26,12 +23,10 @@ export WRF_NMM_CORE=0
 export WRF_CHEM=0
 
 # NetCDF
-export NETCDF_LIB="-L${NETCDF_C_ROOT}/lib -lnetcdf -L${NETCDF_FORTRAN_ROOT}/lib -lnetcdff -Wl,-rpath,${NETCDF_C_ROOT}/lib"
-export NETCDF_INC="-I${NETCDF_C_ROOT}/include -I${NETCDF_FORTRAN_ROOT}/include"
-export NETCDF="${USER_PATH}/privat/netcdf"
-export PATH=${NETCDF}/bin:${PATH}
-export LIBRARY_PATH=$NETCDF_C_ROOT/lib:$LIBRARY_PATH
-export LIBRARY_PATH=$NETCDF_FORTRAN_ROOT/lib:$LIBRARY_PATH
+export NETCDF="${USER_PATH}/path/to/netcdf"
+export NETCDF_LIB="-L${NETCDF}/lib -lnetcdf -lnetcdff -Wl,-rpath,${NETCDF}/lib"
+export NETCDF_INC="-I${NETCDF_C_ROOT}/include"
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${NETCDF}/lib
 
 # NetCDF options
 export NETCDF_ROOT=$NETCDF
@@ -39,29 +34,10 @@ export NETCDF_classic=1
 export NETCDF4=1
 
 # HDF5
-export HDF5="${USER_PATH}/privat/hdf5"
-export DEP_LIB_PATH=-L${HDF5}/lib
-export PATH=${HDF5}/bin:${PATH}
+export HDF5="${USER_PATH}/path/to/hdf5"
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HDF5}/lib
 
 # Libraries
 export DIR="${USER_PATH}/WRF/Build_WRF/LIBRARIES"
 export JASPERLIB=${DIR}/grib2/lib
 export JASPERINC=${DIR}/grib2/include
-export PATH=${DIR}/mpich/bin:${PATH}
-
-# Compilers
-export CC=icc
-export CXX=icpc
-export CFLAGS='-O3 -xHost -ip -no-prec-div -static-intel'
-export CXXFLAGS='-O3 -xHost -ip -no-prec-div -static-intel'
-
-export F77=ifort
-export FC=ifort
-export F90=ifort
-export FFLAGS='-O3 -xHost -ip -no-prec-div -static-intel'
-
-export CPP='icc -E'
-export CXXCPP='icpc -E'
-
-export LDFLAGS=-L${DIR}/grib2/lib
-export CPPFLAGS=-I${DIR}/grib2/include
