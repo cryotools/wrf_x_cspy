a5ca2d5ca7580d5fe18c342a8c2675500ff19e9d
gampnico <45390064+gampnico@users.noreply.github.com>
2024-12-20 07:44:02 +0100

    build: update build scripts
    Removes build_wrf_full.sh

--- src/patch_wrfxcspy/build_wrf.sh
+++ src/patch_wrfxcspy/build_wrf.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -l
 # Copyright 2024 WRFxCSPY Contributors
 
-# Ensure COSIPY_API, NETCDF, HDF5, and JASPER_DIR point to the correct paths.
+# Ensure COSIPY_API, NETCDF, HDF5, and JASPER_ROOT point to the correct paths.
 
 # Load HPC modules
 module purge
-module load intelmpi/2021.6.0 intel/2021.4.0
 module load user-spack
-module load perl/5.36 perl-time-piece perl-path-tiny
+module load intelmpi/2021.6.0 intel/2021.4.0
+module load perl-time-piece perl-path-tiny
+module load jasper
 module load time
 
 # Set library paths here
@@ -15,7 +16,7 @@ export COSIPY_API="/path/to/WRF/COUPLER"
 export COSIPY_DIR="/path/to/WRF/cosipy"
 export NETCDF="/path/to/netcdf"
 export HDF5="/path/to/hdf5"
-export JASPER_DIR="/path/to/jasper"
+# export JASPER_ROOT="/path/to/jasper"
 
 # Coupler
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${COSIPY_API}
@@ -32,12 +33,12 @@ export NETCDF_INC="-I${NETCDF_C_ROOT}/include"
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${NETCDF}/lib
 
 # NetCDF options
-export NETCDF_classic=1
+export NETCDF_classic=0
 export NETCDF4=1
 
 # HDF5
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HDF5}/lib
 
 # Libraries
-export JASPERLIB=${DIR}/grib2/lib
-export JASPERINC=${DIR}/grib2/include
+export JASPERLIB=${JASPER_DIR}/lib
+export JASPERINC=${JASPER_DIR}/include
